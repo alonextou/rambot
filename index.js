8581f3ab74fcadbebd2feed8a76b23e4a6b32d3e
Alex Crawford <alex.crawford@codingzeal.com>
2016-05-23 09:38:06 -0700

    Create bot to return solve function

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+const solve = require('rambo').solve
+const solution = solve("a", ["a"])
+console.log(solution.name)
+
+app.get('/', function (req, res) {
+  const token = req.param('token') || ''
+  let text = req.param('text') || ''
+  text = text.replace(/“|”/g, '"')
+  text = text.replace(/‘|’/g, "'")
+  const textArr = text.split('|')
+
+  try {
+    value1 = eval(textArr[0])
+    value2 = eval(textArr[1])
+  } catch (e) {
+    return res.json('Invalid input.')
+  }  
+
+  const solve = require('rambo').solve
+  const solution = solve(value1, value2)
+  
+  if(solution) {
+    return res.json(solution.name)
+  } else {
+    return res.json('No solution found.')
+  }
+});
+
+app.listen(3000, function () {
+  console.log('Rambot running on port 3000');
+});
+
